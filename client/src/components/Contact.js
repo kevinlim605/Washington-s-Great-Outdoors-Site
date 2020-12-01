9ebc7e96d098205d9c33bdc769a02f47c9b5e24b
Kevin Lim <kevinlim605@gmail.com>
2020-11-30 18:54:22 -0800

    started forms for contact us

--- client/src/components/Contact.js
+++ client/src/components/Contact.js
@@ -1,13 +1,41 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { Formik, Form, Field } from "formik";
+import { Container, Row, Col, FormGroup, Label } from "reactstrap";
 
 function Contact() {
     return( 
-        <div>
+        <Container>
             <Link to="/home">
                 Home
             </Link>
-        </div>
+            <Row>
+                <Col>
+                    <h1>Contact Us</h1>
+                    <h4>Send Us Your Feedback!</h4>
+                </Col>
+            </Row>
+            <Formik>
+                <Form>
+                    <Row className="form-group">
+                        <Col xs={{size: 2}} className="my-auto">
+                            <Label htmlFor="firstname">First Name:</Label>
+                        </Col>
+                        <Col xs={{size: 3}}>
+                            <Field className="form-control" type="text" name="firstname" placeholder="first name"></Field>
+                        </Col>
+                    </Row>
+                    <Row className="form-group">
+                        <Col xs={{size: 2}} className="my-auto">
+                            <Label htmlFor="lastname">Last Name:</Label>
+                        </Col>
+                        <Col xs={{size: 3}}>
+                            <Field className="form-control" type="text" name="lastname" placeholder="last name"></Field>
+                        </Col>
+                    </Row>
+                </Form>
+            </Formik>
+        </Container>
     );
 }
 
